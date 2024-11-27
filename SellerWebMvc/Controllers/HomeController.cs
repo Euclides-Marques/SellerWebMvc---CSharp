0bfe3734e587950ed0c8c7a3c1dfd258f31c25f0
Euclides Marques <euclidesmarques270@gmail.com>
2024-11-26 22:06:35 -0300

    First controller and Razor pages test

--- SellerWebMvc/Controllers/HomeController.cs
+++ SellerWebMvc/Controllers/HomeController.cs
@@ -17,7 +17,8 @@ namespace SellerWebMvc.Controllers
 
         public IActionResult About()
         {
-            ViewData["Message"] = "Your application description page.";
+            ViewData["Message"] = "Salles Web MVC App from C# Course";
+            ViewData["Aluno"] = "Euclides";
 
             return View();
         }
