dedb7555d53e270adb8713925f58650180c3a196
Euclides Marques <euclidesmarques270@gmail.com>
2024-11-30 14:53:02 -0300

    Asynchronous operation using Tasks (async, await)

--- SellerWebMvc/Services/DepartmentService.cs
+++ SellerWebMvc/Services/DepartmentService.cs
@@ -2,6 +2,8 @@
 using SellerWebMvc.Models.ViewModels;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
 
 namespace SellerWebMvc.Services
 {
@@ -14,9 +16,9 @@ namespace SellerWebMvc.Services
             _context = context;
         }
 
-        public List<Department> FindAll()
+        public async Task<List<Department>> FindAllAsync()
         {
-            return _context.Department.OrderBy(x => x.Name).ToList();
+            return await _context.Department.OrderBy(x => x.Name).ToListAsync();
         }
     }
 }
