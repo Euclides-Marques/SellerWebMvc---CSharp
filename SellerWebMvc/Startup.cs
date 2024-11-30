29fcd0b4234288938900a9a3cf9b79dcac30909a
Euclides Marques <euclidesmarques270@gmail.com>
2024-11-30 15:35:45 -0300

    Implementing simple search

--- SellerWebMvc/Startup.cs
+++ SellerWebMvc/Startup.cs
@@ -48,6 +48,8 @@ namespace SellerWebMvc
             services.AddScoped<SellerService>();
 
             services.AddScoped<DepartmentService>();
+
+            services.AddScoped<SalesRecordService>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
